65e62b97ac00ee171bb92b5ba62060eb81cdcd06
Matthew Struble <matt@vevox.io>
2016-11-16 14:45:23 -0800

    Added command to list voice channels with ID

--- src/modules/general.js
+++ src/modules/general.js
@@ -6,11 +6,27 @@ exports.desc = 'General and misc commands';
 exports.install = () => {
 
   // Register "hello" and "ping".
-  Bot.registerCommand(/^hello(?:\W)?$/i, msg => {
+  Bot.registerCommand(/^hello(?:\S)?$/i, msg => {
     msg.channel.sendMessage('Hi there, <@' + msg.author.id + '>');
   });
-  Bot.registerCommand(/^ping(?:\W)?$/i, msg => {
+  Bot.registerCommand(/^ping(?:\S)?$/i, msg => {
     msg.channel.sendMessage('Pong!');
   });
+
+  // List voice ids
+  Bot.registerCommand(/^(?:list )?voice channels$/i, msg => {
+    let voiceChannels = { };
+
+    msg.guild.channels.forEach((channel, channelID) => {
+      if (channel.type === 'voice')
+        voiceChannels[channelID] = {
+          name: channel.name,
+          limit: channel.userLimit
+        };
+    });
+    msg.channel.sendCode('json', JSON.stringify(voiceChannels, null, '\t'));
+
+  });
+
 
 };
