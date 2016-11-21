20716cc949832aa1ed5620c7c62ae2d21c8a8a47
Matthew Struble <matt@vevox.io>
2016-11-21 13:21:11 -0800

    Changed the intro message a bit

--- src/modules/general.js
+++ src/modules/general.js
@@ -6,8 +6,12 @@ exports.desc = 'General and misc commands';
 exports.install = () => {
 
   // Register "hello" and "ping".
-  Bot.registerCommand(/^hello(?:\S)?$/i, msg => {
-    msg.channel.sendMessage('Hi there, <@' + msg.author.id + '>');
+  Bot.registerCommand(/^introduce yourself$/i, msg => {
+    msg.channel.sendMessage(
+      '"*Hello: my name is Iota. ' +
+      'I am a personality construct designed and developed by Maya to help assist personnel in ' +
+      'everyday activites. Is there anything I can do for you?*"');
+    msg.channel.sendMessage('- Iota Personality Construct, Shadows Of Maya :: <http://wiki.vevox.io/lore/som/iota>');
   });
   Bot.registerCommand(/^ping(?:\S)?$/i, msg => {
     msg.channel.sendMessage('Pong!');
